fec01a9dba23964cc4999b7f95c15ac6873c66ec
joaopedro13579 <joao.de.souza2021@hotmail.com>
2023-06-07 15:35:47 -0300

    32commit

--- rainhas.hpp
+++ rainhas.hpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct rainha; 
 int codigo(string teste);
+bool validateBoard(const vector<rainha>& queens);
+bool checkAttack(const rainha& q1, const rainha& q2);
